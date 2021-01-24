9bd0528c1615a7c7235f2ea6fed75e1e6525bd69
Kanin Temsrisuk <kaifa2910@msn.com>
2021-01-24 11:19:05 +0700

    Create Array_Fill_Amount.cs

---
+++ 01-Array-Fill-Amount/Array_Fill_Amount.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Monster_Gunner_Bubble : MonoBehaviour
+{
+    float m_ArrayAmount { get { return fillObjs.Length - 1; } }
+
+    [SerializeField] GameObject[] fillObjs;
+
+    public void SetFillAmount(float fillAmount)
+    {
+        //Debug.Log("Set fill amount >> fillAmount: " + fillAmount);
+        //Debug.Log("Set fill amount >> m_ArrayAmount: " + m_ArrayAmount);
+
+        for (int i = 0; i < fillObjs.Length; i++)
+        {
+            float c = Mathf.Clamp(i / m_ArrayAmount, 0.01f, 1.0f);
+
+            //Debug.Log("Set fill amount >> i: " + i + ", c: " + c);
+
+            if (fillAmount >= c)
+                fillObjs[i].SetActive(true);
+            else
+                fillObjs[i].SetActive(false);
+        }
+    }
+}
